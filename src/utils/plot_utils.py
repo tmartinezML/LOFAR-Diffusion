49c1288ad8a9df8b75afdf63f455ed1103980be3
TobiVM <tmartinez@hs.uni-hambiurg.de>
2024-04-09 12:10:03 +0000

    Fmax conditioning eval

--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -167,6 +167,59 @@ def plot_image_grid_from_file(path, n_rows=5, n_cols=None, save=False,
         imgs, suptitle=path.stem, savefig=savefig, titles=titles, **kwargs
     )
 
+
+def metric_peek(metric, edges, images,
+                names=None, n_examples=10, metric_name='Metric'
+                ):
+    # Find indices of the bins to which each value in input array belongs.
+    binned_idxs = np.digitize(metric, edges)
+
+    # Go through each bin
+    for i_bin in np.unique(binned_idxs):
+
+        # Print the bin range
+        if i_bin == len(edges):
+            suptitle = f'Bin {i_bin} - {edges[i_bin-1]:.2f} <= {metric_name}'
+        else:
+            suptitle = (
+                f'Bin {i_bin} - '
+                f'{edges[i_bin-1]:.2f} <= {metric_name} < {edges[i_bin]:.2f}'
+            )
+
+        # Get the indices of the images in the bin
+        img_idxs = np.argwhere(binned_idxs == i_bin).flatten()
+
+        # Randomly select n_examples from the bin
+        if len(img_idxs) > n_examples:
+            img_idxs = np.random.choice(img_idxs, n_examples, replace=False)
+
+        # If there are no images in the bin, continue to the next bin
+        if not len(img_idxs):
+            continue
+
+        # Plot the images
+        n_col = min(len(img_idxs), 5)
+        n_row = int(np.ceil(len(img_idxs) / n_col))
+        fig, axes = plt.subplots(
+            n_row, n_col, dpi=150, figsize=(3 * n_col, 3 * n_row + 0.75)
+        )
+        if len(img_idxs) == 1:
+            axes = np.array([axes])
+        for i, img in enumerate(images[img_idxs]):
+            ax = axes.flatten()[i]
+            ax.imshow(img)
+            value = (
+                metric.values[img_idxs[i]] if hasattr(metric, "values")
+                else metric[img_idxs[i]]
+            )
+            ax.set_title(
+                f'{names[img_idxs[i]] if names is not None else img_idxs[i]}\n'
+                f'{metric_name} = {value:.2f}'
+            )
+            ax.axis('off')
+        fig.suptitle(suptitle)
+        fig.show()
+
 # -----------------------------#
 # Image Metrics
 # -----------------------------#
