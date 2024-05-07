a040217fbac676ea47fc55433e0741154ce5dbb7
TobiVM <tmartinez@hs.uni-hamburg.de>
2024-05-07 11:46:10 +0200

    -

--- src/analysis/bdsf_analysis.py
+++ src/analysis/bdsf_analysis.py
@@ -509,7 +509,7 @@ if __name__ == "__main__":
     arguments = parser.parse_args()
 
     data_path = Path(
-        "/hs/fs08/data/group-brueggen/tmartinez/diffusion/analysis_results/PowerEMA/PowerEMA_samples_ph-sigma=0.07_10000_guidance_strength=1.00e-01.pt"
+        "/hs/babbage/data/group-brueggen/tmartinez/diffusion/analysis_results/Data_Augmented/Data_Augmented_samples_10000_guidance_strength=1.00e-01.pt"
     )
     out_folder = data_path.parent
 
